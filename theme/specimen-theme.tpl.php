d7403cb817ef9da40933920a23a4da9c991f96cd
Islandora User <islandora@upeikerrlab.com>
2014-04-04 15:31:10 -0300

    update theme

--- theme/specimen-theme.tpl.php
+++ theme/specimen-theme.tpl.php
@@ -19,26 +19,23 @@ echo "</pre>";
 
 <div class="islandora-specimen-object islandora">
   <div class ="specimen_image">
-    <div class="m-btn-group">
-      <a href="/islandora/object/islandora%3A293/manage/overview/ingest" class="m-btn green">Add a Specimen</a>
-    </div>
     <table>
     <tr>
     <td><?php if (isset($variables['islandora_thumbnail_img'])): ?><?php print $variables['islandora_thumbnail_img'] ?><?php endif; ?></td>
     <td><table style="width:300px">
       <tr><th>Taxonomic Information</th></tr>
-      <tr><td>Phylum: <?php print $variables['taxonomy']['phylum'] ?></td></tr>
-      <tr><td>Subphylum: <?php print $variables['taxonomy']['subphylum'] ?></td></tr>
-      <tr><td>Class: <?php print $variables['taxonomy']['class'] ?></td></tr>
-      <tr><td>Order: <?php print $variables['taxonomy']['order'] ?></td></tr>
-      <tr><td>Family: <?php print $variables['taxonomy']['family'] ?></td></tr>
-      <tr><td>Genus: <?php print $variables['taxonomy']['genus'] ?></td></tr>
-      <tr><td>Species: <?php print $variables['taxonomy']['species'] ?></td></tr>
+      <?php if (isset ($variables['taxonomy']['phylum'])):?><tr><td>Phylum: <?php print $variables['taxonomy']['phylum'] ?></td></tr><?php endif; ?>
+      <?php if (isset ($variables['taxonomy']['subphylum'])):?><tr><td>Subphylum: <?php print $variables['taxonomy']['subphylum'] ?></td></tr><?php endif; ?>
+      <?php if (isset ($variables['taxonomy']['class'])):?><tr><td>Class: <?php print $variables['taxonomy']['class'] ?></td></tr><?php endif; ?>
+      <?php if (isset ($variables['taxonomy']['order'])):?><tr><td>Order: <?php print $variables['taxonomy']['order'] ?></td></tr><?php endif; ?>
+      <?php if (isset ($variables['taxonomy']['family'])):?><tr><td>Family: <?php print $variables['taxonomy']['family'] ?></td></tr><?php endif; ?>
+      <?php if (isset ($variables['taxonomy']['genus'])):?><tr><td>Genus: <?php print $variables['taxonomy']['genus'] ?></td></tr><?php endif; ?>
+      <?php if (isset ($variables['taxonomy']['species'])):?><tr><td>Species: <?php print $variables['taxonomy']['species'] ?></td></tr><?php endif; ?>
     </table></td>
     <td><table style="width:300px">
       <tr><th>Geographical Information</th></tr>
-      <tr><td>Site Name: <?php print $variables['geography']['siteName'] ?></td></tr>
-      <tr><td>Location: <?php print $variables['geography']['north'] ?>N, <?php print $variables['geography']['west'] ?>W</td></tr>
+      <?php if (isset ($variables['geography']['siteName'])):?><tr><td>Site Name: <?php print $variables['geography']['siteName'] ?></td></tr><?php endif; ?>
+      <?php if (isset ($variables['geography']['north'])):?><tr><td>Location: <?php print $variables['geography']['north'] ?>N, <?php print $variables['geography']['west'] ?>W</td></tr><?php endif; ?>
     </table></td>
     </table>
   </div>
