934c00d08dd90c5afdafae92bd1d1377d34c4872
Brent Mitton <brent.mitton@gmail.com>
2014-04-15 01:40:15 -0300

    those 5 closing parens were important

--- theme/specimen-theme.tpl.php
+++ theme/specimen-theme.tpl.php
@@ -48,10 +48,10 @@ echo "</pre>";*/
   <table>
     <tbody>
       <tr>
-        <td><img src="<?php print (array_key_exists('img_url', $variables)  ? $variables['img_url']:""?>" width="150"></td>
-        <td><img src="<?php print (array_key_exists('img_url1', $variables) ? $variables['img_url1']:""?>" width="150"></td>
-        <td><img src="<?php print (array_key_exists('img_url2', $variables) ? $variables['img_url2']:""?>" width="150"></td>
-        <td><img src="<?php print (array_key_exists('img_url3', $variables) ? $variables['img_url3']:""?>" width="150"></td>
+        <td><img src="<?php print (array_key_exists('img_url', $variables)) ? $variables['img_url']:""?>" width="150"></td>
+        <td><img src="<?php print (array_key_exists('img_url1', $variables)) ? $variables['img_url1']:""?>" width="150"></td>
+        <td><img src="<?php print (array_key_exists('img_url2', $variables)) ? $variables['img_url2']:""?>" width="150"></td>
+        <td><img src="<?php print (array_key_exists('img_url3', $variables)) ? $variables['img_url3']:""?>" width="150"></td>
         <td><img src="<?php print (array_key_exists('img_url4', $variables) ? $variables['img_url4']:""?>" width="150"></td>
       </tr>
     </tbody>
