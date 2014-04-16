cf8d64aff392e5c90556072b57b41470fc7befd1
Brent Mitton <brent.mitton@gmail.com>
2014-04-16 09:51:59 -0300

    good enough

--- theme/specimen-theme.tpl.php
+++ theme/specimen-theme.tpl.php
@@ -105,7 +105,7 @@ echo "</pre>";*/
   <table>
         <tbody>
         <tr>
-          <th style="border: none;">
+          <th>
             <div class="m-btn green"><?php print l('Manage Related Projects', "islandora/object/{$variables['object_id']}/manage_lab_object_projects") ?></div>
           </th>
         </tr>
